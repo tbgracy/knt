816953aaa6aa337184f7a876a133eca78054e074
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-04-26 10:08:15 +0300

    fix: Camera Switch bug
    I forgot to put `dispose` on the right place

--- lib/pages/counting_page.dart
+++ lib/pages/counting_page.dart
@@ -1,7 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:knt/providers.dart';
 
 import '../constants.dart';
 import '../models/counttype_enum.dart';
@@ -27,14 +26,14 @@ class _CountingPageState extends ConsumerState<CountingPage> {
   late List<CameraDescription> _cameras;
   late CameraDescription _currentCamera;
   late CameraController _cameraController;
-  // late Future<void> _initializeControllerFuture;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     _cameras = widget.cameras;
     _cameraController = CameraController(_cameras[1], ResolutionPreset.medium);
     _currentCamera = _cameras[1];
-    // _initializeControllerFuture = _cameraController.initialize();
+    _initializeControllerFuture = _cameraController.initialize();
     super.initState();
   }
 
@@ -51,26 +50,28 @@ class _CountingPageState extends ConsumerState<CountingPage> {
   void _toggleCameraLens() async {
     final lensDirection = _cameraController.description.lensDirection;
     if (lensDirection == CameraLensDirection.front) {
-      setState(() {
+      // setState(() {
         _currentCamera = _cameras.firstWhere(
             (element) => element.lensDirection == CameraLensDirection.back);
-        // ref.invalidate(newCameraControllerProvider);
-      });
+      // });
     } else {
-      setState(() {
+      // setState(() {
         _currentCamera = _cameras.firstWhere(
             (element) => element.lensDirection == CameraLensDirection.front);
-        // ref.invalidate(newCameraControllerProvider);
-      });
+      // });
     }
-    ref.invalidate(newCameraControllerProvider);
+    _cameraController =
+        CameraController(_currentCamera, ResolutionPreset.medium);
+    setState(
+      () {
+        _initializeControllerFuture = _cameraController.initialize();
+      },
+    );
+    // ref.invalidate(newCameraControllerProvider);
   }
 
   @override
   Widget build(BuildContext context) {
-    final cameraController =
-        ref.watch(newCameraControllerProvider(_currentCamera));
-
     return Scaffold(
       backgroundColor: colors[3],
       appBar: AppBar(
@@ -99,24 +100,25 @@ class _CountingPageState extends ConsumerState<CountingPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            cameraController.when(
-              data: (controllerData) {
-                return Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(30)),
-                  child: CameraPreview(controllerData),
-                );
-              },
-              error: (e, s) {
-                return Text(e.toString());
-              },
-              loading: () {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.6,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: FutureBuilder(
+                    future: _initializeControllerFuture,
+                    builder: (ctx, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return CameraPreview(_cameraController);
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+              ),
             ),
             Positioned(
               bottom: 40,
